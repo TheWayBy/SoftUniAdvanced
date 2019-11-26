2bcd8038ae6b209ea769ff1ca4d5505879d11e49
Viktoria <viktoriqvveleva@gmail.com>
2019-11-26 21:41:12 +0200

    upload todo project

---
+++ 02.ToDoList/ToDoList/Data/ToDoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoDbContext:DbContext
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=BandDb;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionBuilder)
+        {
+            OptionBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public object Task { get; internal set; }
+    }
+}
